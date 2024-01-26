cd9ea8266490cd06b1e297ecbed8b190a8ae630a
Irina <ivzhyurkevich@gmail.com>
2024-01-26 13:12:19 +0300

    fix erro boundary

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import {ErrorBoundary} from "next/dist/client/components/error-boundary";
 
 export default function App({Component, pageProps}) {
-  return;
-  <ErrorBoundary>
-    <Component {...pageProps} />
-  </ErrorBoundary>;
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  );
 }
